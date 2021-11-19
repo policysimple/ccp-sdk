1d9f21b0d1f4f9132982200c2fbf857ab9361613
igordevcuemby <igor.ariza@cuemby.com>
2021-11-19 15:16:53 -0500

    feat: refactor

--- docker.sh
+++ docker.sh
@@ -1,11 +1,12 @@
 # Docker commands
 NAME_IMAGE=proto-compiler
 NAME_CONTAINER=proto-compiler
-DIR_LOCAL=`pwd`
+DIR_LOCAL='C:\Users\madel\Documents\Cuemby\github.com\cuemby\ccp-sdk'
 docker build --tag $NAME_IMAGE .
 
 docker rm -fv $NAME_CONTAINER
 
-docker run -dti --name $NAME_CONTAINER -v $DIR_LOCAL/:/temp/buf-gen $NAME_IMAGE
+docker run -dti --name $NAME_CONTAINER -v $DIR_LOCAL:/temp/buf-gen $NAME_IMAGE
+
 
 docker exec -ti $NAME_CONTAINER bash
