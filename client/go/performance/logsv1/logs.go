562c401115350b2248a272f81a8923c2b231c33e
Igor Ariza <igor.ariza@cuemby.com>
2022-03-30 09:51:35 -0500

    feat (ccp-sdk): Client performance logs and metrics integrate

--- client/go/performance/logsv1/logs.go
+++ client/go/performance/logsv1/logs.go
@@ -1,16 +1,22 @@
 package logsclientv1
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"os"
 	"sync"
+	"time"
 
 	logsgpkgv1 "github.com/cuemby/ccp-sdk/gen/go/performance/logs/v1alpha1"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
-var client logsgpkgv1.LogsAgentAPIServiceClient
+var client logsgpkgv1.LogsAPIServiceClient
 var doOnce sync.Once
 
 var logsServiceUri string
@@ -18,15 +24,40 @@ var logsServiceTimeout string
 
 func init() {
 	doOnce.Do(func() {
-		logsServiceTimeout = os.Getenv("LOGS_SERVICE_TIMEOUT")
+		logsServiceTimeout = os.Getenv("PERFORMANCE_SERVICE_TIMEOUT")
 		if logsServiceTimeout == "" {
 			logsServiceTimeout = "30s"
 		}
-		logsServiceUri = os.Getenv("RUNTIME_SERVICE_URI")
+		logsServiceUri = os.Getenv("PERFORMANCE_SERVICE_URI")
 		con, err := grpc.Dial(logsServiceUri, grpc.WithTransportCredentials(insecure.NewCredentials()))
 		if err != nil {
 			panic(err)
 		}
-		client = logsgpkgv1.NewLogsAgentAPIServiceClient(con)
+		client = logsgpkgv1.NewLogsAPIServiceClient(con)
 	})
+}
+
+func GetLogs(getLogs *logsgpkgv1.GetLogsRequest) (response *logsgpkgv1.GetLogsResponse, err error) {
+	d, err := time.ParseDuration(logsServiceTimeout)
+	if err != nil {
+		return
+	}
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(d))
+	defer cancel()
+
+	response, err = client.GetLogs(ctx, &logsgpkgv1.GetLogsRequest{
+		Labels:     getLogs.Labels,
+		Containers: getLogs.Containers,
+		Range:      getLogs.Range,
+		Size:       getLogs.Size,
+		NextPageId: getLogs.NextPageId,
+	})
+	if err != nil {
+		log.Printf("%s: %v", "Error get logs", err)
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			fmt.Sprintf("%s: %v", "Error get Logs", err),
+		)
+	}
+	return response, nil
 }
