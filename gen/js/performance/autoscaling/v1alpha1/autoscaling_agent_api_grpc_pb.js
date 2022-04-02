ddb8a8dd53b7653b43e8cdeead740add76b83d1d
Igor Ariza <igor.ariza@cuemby.com>
2022-04-02 14:53:29 -0500

    fix (ccp-sdk): Integrate Autosaling

---
+++ gen/js/performance/autoscaling/v1alpha1/autoscaling_agent_api_grpc_pb.js
@@ -0,0 +1,177 @@
+// GENERATED CODE -- DO NOT EDIT!
+
+'use strict';
+var grpc = require('grpc');
+var performance_autoscaling_v1alpha1_autoscaling_agent_api_pb = require('../../../performance/autoscaling/v1alpha1/autoscaling_agent_api_pb.js');
+var performance_autoscaling_v1alpha1_autoscaling_pb = require('../../../performance/autoscaling/v1alpha1/autoscaling_pb.js');
+
+function serialize_performance_autoscaling_v1alpha1_AgentCreateAutoscalingRequest(arg) {
+  if (!(arg instanceof performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentCreateAutoscalingRequest)) {
+    throw new Error('Expected argument of type performance.autoscaling.v1alpha1.AgentCreateAutoscalingRequest');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_performance_autoscaling_v1alpha1_AgentCreateAutoscalingRequest(buffer_arg) {
+  return performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentCreateAutoscalingRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_performance_autoscaling_v1alpha1_AgentCreateAutoscalingResponse(arg) {
+  if (!(arg instanceof performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentCreateAutoscalingResponse)) {
+    throw new Error('Expected argument of type performance.autoscaling.v1alpha1.AgentCreateAutoscalingResponse');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_performance_autoscaling_v1alpha1_AgentCreateAutoscalingResponse(buffer_arg) {
+  return performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentCreateAutoscalingResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_performance_autoscaling_v1alpha1_AgentDeleteAutoscalingRequest(arg) {
+  if (!(arg instanceof performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentDeleteAutoscalingRequest)) {
+    throw new Error('Expected argument of type performance.autoscaling.v1alpha1.AgentDeleteAutoscalingRequest');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_performance_autoscaling_v1alpha1_AgentDeleteAutoscalingRequest(buffer_arg) {
+  return performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentDeleteAutoscalingRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_performance_autoscaling_v1alpha1_AgentDeleteAutoscalingResponse(arg) {
+  if (!(arg instanceof performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentDeleteAutoscalingResponse)) {
+    throw new Error('Expected argument of type performance.autoscaling.v1alpha1.AgentDeleteAutoscalingResponse');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_performance_autoscaling_v1alpha1_AgentDeleteAutoscalingResponse(buffer_arg) {
+  return performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentDeleteAutoscalingResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_performance_autoscaling_v1alpha1_AgentGetAutoscalingRequest(arg) {
+  if (!(arg instanceof performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentGetAutoscalingRequest)) {
+    throw new Error('Expected argument of type performance.autoscaling.v1alpha1.AgentGetAutoscalingRequest');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_performance_autoscaling_v1alpha1_AgentGetAutoscalingRequest(buffer_arg) {
+  return performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentGetAutoscalingRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_performance_autoscaling_v1alpha1_AgentGetAutoscalingResponse(arg) {
+  if (!(arg instanceof performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentGetAutoscalingResponse)) {
+    throw new Error('Expected argument of type performance.autoscaling.v1alpha1.AgentGetAutoscalingResponse');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_performance_autoscaling_v1alpha1_AgentGetAutoscalingResponse(buffer_arg) {
+  return performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentGetAutoscalingResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_performance_autoscaling_v1alpha1_AgentListAutoscalingRequest(arg) {
+  if (!(arg instanceof performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentListAutoscalingRequest)) {
+    throw new Error('Expected argument of type performance.autoscaling.v1alpha1.AgentListAutoscalingRequest');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_performance_autoscaling_v1alpha1_AgentListAutoscalingRequest(buffer_arg) {
+  return performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentListAutoscalingRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_performance_autoscaling_v1alpha1_AgentListAutoscalingResponse(arg) {
+  if (!(arg instanceof performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentListAutoscalingResponse)) {
+    throw new Error('Expected argument of type performance.autoscaling.v1alpha1.AgentListAutoscalingResponse');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_performance_autoscaling_v1alpha1_AgentListAutoscalingResponse(buffer_arg) {
+  return performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentListAutoscalingResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_performance_autoscaling_v1alpha1_AgentUpdateAutoscalingRequest(arg) {
+  if (!(arg instanceof performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentUpdateAutoscalingRequest)) {
+    throw new Error('Expected argument of type performance.autoscaling.v1alpha1.AgentUpdateAutoscalingRequest');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_performance_autoscaling_v1alpha1_AgentUpdateAutoscalingRequest(buffer_arg) {
+  return performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentUpdateAutoscalingRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_performance_autoscaling_v1alpha1_AgentUpdateAutoscalingResponse(arg) {
+  if (!(arg instanceof performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentUpdateAutoscalingResponse)) {
+    throw new Error('Expected argument of type performance.autoscaling.v1alpha1.AgentUpdateAutoscalingResponse');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_performance_autoscaling_v1alpha1_AgentUpdateAutoscalingResponse(buffer_arg) {
+  return performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentUpdateAutoscalingResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+
+var AutoscalingAgentAPIServiceService = exports.AutoscalingAgentAPIServiceService = {
+  agentCreateAutoscaling: {
+    path: '/performance.autoscaling.v1alpha1.AutoscalingAgentAPIService/AgentCreateAutoscaling',
+    requestStream: true,
+    responseStream: true,
+    requestType: performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentCreateAutoscalingRequest,
+    responseType: performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentCreateAutoscalingResponse,
+    requestSerialize: serialize_performance_autoscaling_v1alpha1_AgentCreateAutoscalingRequest,
+    requestDeserialize: deserialize_performance_autoscaling_v1alpha1_AgentCreateAutoscalingRequest,
+    responseSerialize: serialize_performance_autoscaling_v1alpha1_AgentCreateAutoscalingResponse,
+    responseDeserialize: deserialize_performance_autoscaling_v1alpha1_AgentCreateAutoscalingResponse,
+  },
+  agentUpdateAutoscaling: {
+    path: '/performance.autoscaling.v1alpha1.AutoscalingAgentAPIService/AgentUpdateAutoscaling',
+    requestStream: true,
+    responseStream: true,
+    requestType: performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentUpdateAutoscalingRequest,
+    responseType: performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentUpdateAutoscalingResponse,
+    requestSerialize: serialize_performance_autoscaling_v1alpha1_AgentUpdateAutoscalingRequest,
+    requestDeserialize: deserialize_performance_autoscaling_v1alpha1_AgentUpdateAutoscalingRequest,
+    responseSerialize: serialize_performance_autoscaling_v1alpha1_AgentUpdateAutoscalingResponse,
+    responseDeserialize: deserialize_performance_autoscaling_v1alpha1_AgentUpdateAutoscalingResponse,
+  },
+  agentDeleteAutoscaling: {
+    path: '/performance.autoscaling.v1alpha1.AutoscalingAgentAPIService/AgentDeleteAutoscaling',
+    requestStream: true,
+    responseStream: true,
+    requestType: performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentDeleteAutoscalingRequest,
+    responseType: performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentDeleteAutoscalingResponse,
+    requestSerialize: serialize_performance_autoscaling_v1alpha1_AgentDeleteAutoscalingRequest,
+    requestDeserialize: deserialize_performance_autoscaling_v1alpha1_AgentDeleteAutoscalingRequest,
+    responseSerialize: serialize_performance_autoscaling_v1alpha1_AgentDeleteAutoscalingResponse,
+    responseDeserialize: deserialize_performance_autoscaling_v1alpha1_AgentDeleteAutoscalingResponse,
+  },
+  agentGetAutoscaling: {
+    path: '/performance.autoscaling.v1alpha1.AutoscalingAgentAPIService/AgentGetAutoscaling',
+    requestStream: true,
+    responseStream: true,
+    requestType: performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentGetAutoscalingRequest,
+    responseType: performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentGetAutoscalingResponse,
+    requestSerialize: serialize_performance_autoscaling_v1alpha1_AgentGetAutoscalingRequest,
+    requestDeserialize: deserialize_performance_autoscaling_v1alpha1_AgentGetAutoscalingRequest,
+    responseSerialize: serialize_performance_autoscaling_v1alpha1_AgentGetAutoscalingResponse,
+    responseDeserialize: deserialize_performance_autoscaling_v1alpha1_AgentGetAutoscalingResponse,
+  },
+  agentListAutoscaling: {
+    path: '/performance.autoscaling.v1alpha1.AutoscalingAgentAPIService/AgentListAutoscaling',
+    requestStream: true,
+    responseStream: true,
+    requestType: performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentListAutoscalingRequest,
+    responseType: performance_autoscaling_v1alpha1_autoscaling_agent_api_pb.AgentListAutoscalingResponse,
+    requestSerialize: serialize_performance_autoscaling_v1alpha1_AgentListAutoscalingRequest,
+    requestDeserialize: deserialize_performance_autoscaling_v1alpha1_AgentListAutoscalingRequest,
+    responseSerialize: serialize_performance_autoscaling_v1alpha1_AgentListAutoscalingResponse,
+    responseDeserialize: deserialize_performance_autoscaling_v1alpha1_AgentListAutoscalingResponse,
+  },
+};
+
+exports.AutoscalingAgentAPIServiceClient = grpc.makeGenericClientConstructor(AutoscalingAgentAPIServiceService);
---
+++ gen/php/Source/V1alpha1/SourceServiceClient.php
@@ -0,0 +1,270 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+namespace Source\V1alpha1;
+
+/**
+ */
+class SourceServiceClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * PROVIDERS
+     * @param \Source\V1alpha1\CreateProviderRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function CreateProvider(\Source\V1alpha1\CreateProviderRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/CreateProvider',
+        $argument,
+        ['\Source\V1alpha1\CreateProviderResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Source\V1alpha1\ListProvidersRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function ListProviders(\Source\V1alpha1\ListProvidersRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/ListProviders',
+        $argument,
+        ['\Source\V1alpha1\ListProvidersResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Source\V1alpha1\GetProviderRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function GetProvider(\Source\V1alpha1\GetProviderRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/GetProvider',
+        $argument,
+        ['\Source\V1alpha1\GetProviderResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Source\V1alpha1\GetOneProviderByNameRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function GetOneProviderByName(\Source\V1alpha1\GetOneProviderByNameRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/GetOneProviderByName',
+        $argument,
+        ['\Source\V1alpha1\GetOneProviderByNameResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Source\V1alpha1\UpdateProviderRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function UpdateProvider(\Source\V1alpha1\UpdateProviderRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/UpdateProvider',
+        $argument,
+        ['\Source\V1alpha1\UpdateProviderResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Source\V1alpha1\DeleteProviderRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function DeleteProvider(\Source\V1alpha1\DeleteProviderRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/DeleteProvider',
+        $argument,
+        ['\Source\V1alpha1\DeleteProviderResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * INTEGRATIONS
+     * @param \Source\V1alpha1\CreateIntegrationRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function CreateIntegration(\Source\V1alpha1\CreateIntegrationRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/CreateIntegration',
+        $argument,
+        ['\Source\V1alpha1\CreateIntegrationResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Source\V1alpha1\ListIntegrationsRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function ListIntegrations(\Source\V1alpha1\ListIntegrationsRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/ListIntegrations',
+        $argument,
+        ['\Source\V1alpha1\ListIntegrationsResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Source\V1alpha1\GetIntegrationRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function GetIntegration(\Source\V1alpha1\GetIntegrationRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/GetIntegration',
+        $argument,
+        ['\Source\V1alpha1\GetIntegrationResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Source\V1alpha1\UpdateIntegrationRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function UpdateIntegration(\Source\V1alpha1\UpdateIntegrationRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/UpdateIntegration',
+        $argument,
+        ['\Source\V1alpha1\UpdateIntegrationResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Source\V1alpha1\DeleteIntegrationRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function DeleteIntegration(\Source\V1alpha1\DeleteIntegrationRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/DeleteIntegration',
+        $argument,
+        ['\Source\V1alpha1\DeleteIntegrationResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Source\V1alpha1\DeleteIntegrationsByOrganizationRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function DeleteIntegrationsByOrganization(\Source\V1alpha1\DeleteIntegrationsByOrganizationRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/DeleteIntegrationsByOrganization',
+        $argument,
+        ['\Source\V1alpha1\DeleteIntegrationsByOrganizationResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * List Repositories providers by integrations
+     * @param \Source\V1alpha1\ListRepositoriesProviderRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function ListRepositoriesProvider(\Source\V1alpha1\ListRepositoriesProviderRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/ListRepositoriesProvider',
+        $argument,
+        ['\Source\V1alpha1\ListRepositoriesProviderResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Source\V1alpha1\GetRepositoryProviderRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function GetRepositoryProvider(\Source\V1alpha1\GetRepositoryProviderRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/GetRepositoryProvider',
+        $argument,
+        ['\Source\V1alpha1\GetRepositoryProviderResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * REPOSITORIES
+     * @param \Source\V1alpha1\CreateRepositoryRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function CreateRepository(\Source\V1alpha1\CreateRepositoryRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/CreateRepository',
+        $argument,
+        ['\Source\V1alpha1\CreateRepositoryResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Source\V1alpha1\ListRepositoriesRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function ListRepositories(\Source\V1alpha1\ListRepositoriesRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/ListRepositories',
+        $argument,
+        ['\Source\V1alpha1\ListRepositoriesResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Source\V1alpha1\GetRepositoryRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function GetRepository(\Source\V1alpha1\GetRepositoryRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/GetRepository',
+        $argument,
+        ['\Source\V1alpha1\GetRepositoryResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Source\V1alpha1\DeleteRepositoryRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function DeleteRepository(\Source\V1alpha1\DeleteRepositoryRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/DeleteRepository',
+        $argument,
+        ['\Source\V1alpha1\DeleteRepositoryResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Source\V1alpha1\UpdateRepositoryRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function UpdateRepository(\Source\V1alpha1\UpdateRepositoryRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/source.v1alpha1.SourceService/UpdateRepository',
+        $argument,
+        ['\Source\V1alpha1\UpdateRepositoryResponse', 'decode'],
+        $metadata, $options);
+    }
+
+}
