fe922d9b94356df5ace03c8dd4e1bb5546db556c
MALEROCHA03 <alejalafafita@gmail.com>
2022-05-19 09:39:12 -0500

    Arocha-fix-Payment

---
+++ client/go/artifacts/projectsv1/projects.go
@@ -0,0 +1,121 @@
+package registryprojectsclientv1
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	bylogs "github.com/cuemby/by-go-utils/pkg/bylogger"
+	clientpkgv1 "github.com/cuemby/ccp-sdk/gen/go/artifacts/projects/v1alpha1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+)
+
+var client clientpkgv1.RegistryProjectsAPIServiceClient
+var doOnce sync.Once
+
+var clientServiceUri string
+var clientServiceTimeout string
+
+func init() {
+	doOnce.Do(func() {
+		clientServiceTimeout = os.Getenv("ARTIFACT_SERVICE_TIMEOUT")
+		if clientServiceTimeout == "" {
+			clientServiceTimeout = "30s"
+		}
+		clientServiceUri = os.Getenv("ARTIFACT_SERVICE_URI")
+		con, err := grpc.Dial(clientServiceUri, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			panic(err)
+		}
+		client = clientpkgv1.NewRegistryProjectsAPIServiceClient(con)
+	})
+}
+
+func CreateProjectRegistry(in *clientpkgv1.CreateProjectRegistryRequest) (response *clientpkgv1.CreateProjectRegistryResponse, err error) {
+	bylogs.LogInfo("Client: Create registry project")
+	d, err := time.ParseDuration(clientServiceTimeout)
+	if err != nil {
+		return
+	}
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(d))
+	defer cancel()
+
+	response, err = client.CreateProjectRegistry(ctx, in)
+
+	if err != nil {
+		bylogs.LogErr("Client: Error create project registry", err)
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			fmt.Sprintf("%s: %v", "Error create project registry", err),
+		)
+	}
+	return response, nil
+}
+
+func ListProjectsRegistry(in *clientpkgv1.ListProjectsRegistryRequest) (response *clientpkgv1.ListProjectsRegistryResponse, err error) {
+	bylogs.LogInfo("Client: List registry projects")
+	d, err := time.ParseDuration(clientServiceTimeout)
+	if err != nil {
+		return
+	}
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(d))
+	defer cancel()
+
+	response, err = client.ListProjectsRegistry(ctx, in)
+
+	if err != nil {
+		bylogs.LogErr("Client: Error list projects registry", err)
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			fmt.Sprintf("%s: %v", "Error list projects registry", err),
+		)
+	}
+	return response, nil
+}
+
+func UpdateProjectRegistry(in *clientpkgv1.UpdateProjectRegistryRequest) (response *clientpkgv1.UpdateProjectRegistryResponse, err error) {
+	bylogs.LogInfo("Client: Update registry project")
+	d, err := time.ParseDuration(clientServiceTimeout)
+	if err != nil {
+		return
+	}
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(d))
+	defer cancel()
+
+	response, err = client.UpdateProjectRegistry(ctx, in)
+
+	if err != nil {
+		bylogs.LogErr("Client: Error Update project registry", err)
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			fmt.Sprintf("%s: %v", "Error Update project registry", err),
+		)
+	}
+	return response, nil
+}
+
+func DeleteProjectRegistry(in *clientpkgv1.DeleteProjectRegistryRequest) (response *clientpkgv1.DeleteProjectRegistryResponse, err error) {
+	bylogs.LogInfo("Client: Delete registry project")
+	d, err := time.ParseDuration(clientServiceTimeout)
+	if err != nil {
+		return
+	}
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(d))
+	defer cancel()
+
+	response, err = client.DeleteProjectRegistry(ctx, in)
+
+	if err != nil {
+		bylogs.LogErr("Client: Error Delete project registry", err)
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			fmt.Sprintf("%s: %v", "Error Delete project registry", err),
+		)
+	}
+	return response, nil
+}
